78777754dc9cd98de54fbfba8cc7c538018b0f7a
inventivetalent <inventivetalent@inventivegames.de>
2015-04-18 02:21:52 +0200

    Initial commit

---
+++ BungeePacketListenerAPI/src/de/inventivegames/packetlistener/bungee/PacketListenerAPI.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2015 Marvin Schäfer (inventivetalent). All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and contributors and should not be interpreted as representing official policies,
+ * either expressed or implied, of anybody else.
+ */
+
+package de.inventivegames.packetlistener.bungee;
+
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import net.md_5.bungee.UserConnection;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.netty.ChannelWrapper;
+import net.md_5.bungee.protocol.DefinedPacket;
+import net.md_5.bungee.protocol.PacketWrapper;
+import de.inventivegames.packetlistener.handler.PacketHandler;
+import de.inventivegames.packetlistener.handler.ReceivedPacket;
+import de.inventivegames.packetlistener.handler.SentPacket;
+
+/**
+ *
+ * © Copyright 2015 inventivetalent
+ *
+ * @author inventivetalent
+ *
+ */
+public class PacketListenerAPI extends Plugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		ProxyServer.getInstance().getPluginManager().registerListener(this, this);
+	}
+
+	@EventHandler
+	public void onJoin(PostLoginEvent e) {
+		addChannel(e.getPlayer());
+
+	}
+
+	@EventHandler
+	public void onQuit(PlayerDisconnectEvent e) {
+		removeChannel(e.getPlayer());
+	}
+
+	private ChannelWrapper getChannel(ProxiedPlayer player) throws Exception {
+		ChannelWrapper channel = (ChannelWrapper) AccessUtil.setAccessible(UserConnection.class.getDeclaredField("ch")).get((UserConnection) player);
+		return channel;
+	}
+
+	public void addChannel(final ProxiedPlayer player) {
+		try {
+			ChannelWrapper channel = getChannel(player);
+			channel.getHandle().pipeline().addBefore("inbound-boss", "packet_listener_player", new ChannelDuplexHandler() {
+
+				@Override
+				public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+					Cancellable cancellable = new Cancellable();
+					Object pckt = msg;
+					if (DefinedPacket.class.isAssignableFrom(msg.getClass())) {
+						pckt = (DefinedPacket) onPacketSend(player, (DefinedPacket) msg, cancellable);
+					}
+					if (PacketWrapper.class.isAssignableFrom(msg.getClass())) {
+						pckt = (PacketWrapper) onPacketSend(player, msg, cancellable);
+					}
+					if (cancellable.isCancelled()) return;
+					super.write(ctx, pckt, promise);
+				}
+
+				@Override
+				public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+					Cancellable cancellable = new Cancellable();
+					Object pckt = msg;
+					if (DefinedPacket.class.isAssignableFrom(msg.getClass())) {
+						pckt = (DefinedPacket) onPacketReceive(player, (DefinedPacket) msg, cancellable);
+					}
+					if (PacketWrapper.class.isAssignableFrom(msg.getClass())) {
+						pckt = (PacketWrapper) onPacketReceive(player, msg, cancellable);
+					}
+					if (cancellable.isCancelled()) return;
+					super.channelRead(ctx, pckt);
+				}
+
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void removeChannel(ProxiedPlayer player) {
+		try {
+			ChannelWrapper channel = getChannel(player);
+			channel.getHandle().pipeline().remove("packet_listener_player");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Object onPacketReceive(ProxiedPlayer p, Object packet, Cancellable cancellable) {
+		if (packet == null) return packet;
+		ReceivedPacket pckt = new ReceivedPacket(packet, cancellable, p);
+		PacketHandler.notifyHandlers(pckt);
+		return pckt.getSourcePacket();
+	}
+
+	public Object onPacketSend(ProxiedPlayer p, Object packet, Cancellable cancellable) {
+		if (packet == null) return packet;
+		SentPacket pckt = new SentPacket(packet, cancellable, p);
+		PacketHandler.notifyHandlers(pckt);
+		return pckt.getSourcePacket();
+	}
+
+}
---
+++ BungeePacketListenerAPI/src/de/inventivegames/packetlistener/handler/Packet.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright 2015 Marvin Schäfer (inventivetalent). All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and contributors and should not be interpreted as representing official policies,
+ * either expressed or implied, of anybody else.
+ */
+
+package de.inventivegames.packetlistener.handler;
+
+import java.lang.reflect.Field;
+
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.protocol.DefinedPacket;
+import net.md_5.bungee.protocol.PacketWrapper;
+import de.inventivegames.packetlistener.bungee.Cancellable;
+
+/**
+ * Base class for sent or received packets
+ *
+ * @see SentPacket
+ * @see ReceivedPacket
+ */
+public abstract class Packet {
+
+	private ProxiedPlayer	player;
+	private Object			packet;
+	private Cancellable		cancel;
+
+	public Packet(Object packet, Cancellable cancel, ProxiedPlayer player) {
+		this.player = player;
+		this.packet = packet;
+		this.cancel = cancel;
+	}
+
+	/**
+	 * Modify a value of the packet
+	 *
+	 * @param field
+	 *            Name of the field to modify
+	 * @param value
+	 *            Value to be assigned to the field
+	 */
+	public void setPacketValue(String field, Object value) {
+		try {
+			Field f = this.packet.getClass().getDeclaredField(field);
+			f.setAccessible(true);
+			f.set(this.packet, value);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Get a value of the packet
+	 *
+	 * @param field
+	 *            Name of the field
+	 * @return current value of the field
+	 */
+	public Object getPacketValue(String field) {
+		Object value = null;
+		try {
+			Field f = this.packet.getClass().getDeclaredField(field);
+			f.setAccessible(true);
+			value = f.get(this.packet);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return value;
+	}
+
+	/**
+	 * @param b
+	 *            if set to <code>true</code> the packet will be cancelled
+	 */
+	public void setCancelled(boolean b) {
+		this.cancel.setCancelled(b);
+	}
+
+	/**
+	 * @return <code>true</code> if the packet has been cancelled
+	 */
+	public boolean isCancelled() {
+		return this.cancel.isCancelled();
+	}
+
+	/**
+	 * @return The receiving or sending player of the packet
+	 *
+	 * @see #hasPlayer()
+	 */
+	public ProxiedPlayer getPlayer() {
+		return this.player;
+	}
+
+	/**
+	 * @return <code>true</code> if the packet has a player
+	 */
+	public boolean hasPlayer() {
+		return this.player != null;
+	}
+
+	/**
+	 * @return The name of the receiving or sending player
+	 *
+	 * @see #hasPlayer()
+	 * @see #getPlayer()
+	 */
+	public String getPlayername() {
+		if (!this.hasPlayer()) return null;
+		return this.player.getName();
+	}
+
+	/**
+	 * Change the packet that is sent
+	 *
+	 * @param packet
+	 *            new packet
+	 */
+	public void setPacket(DefinedPacket packet) {
+		if (this.packet instanceof DefinedPacket) {
+			this.packet = packet;
+		}
+		if (this.packet instanceof PacketWrapper) {
+			this.packet = new PacketWrapper(packet, ((PacketWrapper) this.packet).buf);
+		}
+	}
+
+	/**
+	 * @return the sent or received packet as an Object
+	 */
+	public DefinedPacket getPacket() {
+		if (packet instanceof DefinedPacket) {
+			return (DefinedPacket) packet;
+		}
+		if (packet instanceof PacketWrapper) {
+			return ((PacketWrapper) packet).packet;
+		}
+		return null;
+	}
+
+	/**
+	 * @return the sent or received packet as an Object
+	 */
+	public Object getSourcePacket() {
+		return this.packet;
+	}
+
+	public void setSourcePacket(Object packet) {
+		this.packet = packet;
+	}
+
+	/**
+	 * @return the class name of the sent or received packet
+	 */
+	public String getPacketName() {
+		return this.packet.getClass().getSimpleName();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (this.cancel == null ? 0 : this.cancel.hashCode());
+		result = prime * result + (this.packet == null ? 0 : this.packet.hashCode());
+		result = prime * result + (this.player == null ? 0 : this.player.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (this.getClass() != obj.getClass()) return false;
+		Packet other = (Packet) obj;
+		if (this.cancel == null) {
+			if (other.cancel != null) return false;
+		} else if (!this.cancel.equals(other.cancel)) return false;
+		if (this.packet == null) {
+			if (other.packet != null) return false;
+		} else if (!this.packet.equals(other.packet)) return false;
+		if (this.player == null) {
+			if (other.player != null) return false;
+		} else if (!this.player.equals(other.player)) return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Packet{ " + (this.getClass().equals(SentPacket.class) ? "[> OUT >]" : "[< IN <]") + " " + this.getPacketName() + " " + (this.hasPlayer() ? this.getPlayername() : "#server#") + " }";
+	}
+
+}
